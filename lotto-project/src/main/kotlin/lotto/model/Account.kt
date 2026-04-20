fe7a85b4a7e2bd40e8bd99d21976ad7f6ec5d405
Jaehyun Yoon <yuchem2@gmail.com>
2026-04-20 20:04:43 +0900

    feat: add Account class

---
+++ lotto-project/src/main/kotlin/lotto/model/Account.kt
@@ -0,0 +1,20 @@
+package lotto.model
+
+import kotlin.collections.plusAssign
+
+class Account(
+    private var amount: Int
+) {
+    fun withdraw(amount: Int) {
+        if (this.amount > amount) {
+            throw IllegalArgumentException("잔액보다 출금액이 많습니다.")
+        }
+        this.amount -= amount
+    }
+
+    fun deposit(amount: Int) {
+        this.amount += amount
+    }
+
+    fun getAmount() = amount
+}
