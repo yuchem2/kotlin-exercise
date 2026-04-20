67f69d7962b155afefd5213c6e97c7dc32d8020b
Jaehyun Yoon <yuchem2@gmail.com>
2026-04-20 20:56:35 +0900

    feat: add LottoDraw and LottoRank class

--- lotto-project/src/main/kotlin/lotto/model/LottoTicket.kt
+++ lotto-project/src/main/kotlin/lotto/model/LottoTicket.kt
@@ -5,11 +5,15 @@ import lotto.constant.MIN_NUMBER
 import lotto.constant.TICKET_SIZE
 
 data class LottoTicket(
-    val numbers: List<Int>
+    val numbers: List<Int>,
 ) {
     init {
         require(numbers.size == TICKET_SIZE) { "로또 번호는 ${TICKET_SIZE}개여야 합니다" }
         require(numbers.all { it in MIN_NUMBER..MAX_NUMBER }) { "로또 번호는 ${MIN_NUMBER}와 $MAX_NUMBER 사이여야 합니다" }
         require(numbers.toSet().size == TICKET_SIZE) { "로또 번호에 중복은 없어야 합니다" }
     }
+
+    fun compare(target: LottoTicket): Int = numbers.intersect(target.numbers.toSet()).size
+
+    fun hasBonusNumbers(target: List<Int>): Int = numbers.intersect(target.toSet()).size
 }
