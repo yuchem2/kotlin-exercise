8ce2ace1ec525b69967b702824d786c8b53d7101
Jaehyun Yoon <yuchem2@gmail.com>
2026-04-20 20:59:52 +0900

    fix: typo error in Account class

--- lotto-project/src/main/kotlin/lotto/model/Account.kt
+++ lotto-project/src/main/kotlin/lotto/model/Account.kt
@@ -6,7 +6,7 @@ class Account(
     private var amount: Int
 ) {
     fun withdraw(amount: Int) {
-        if (this.amount > amount) {
+        if (this.amount < amount) {
             throw IllegalArgumentException("잔액보다 출금액이 많습니다.")
         }
         this.amount -= amount
