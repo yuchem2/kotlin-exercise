835762706dd78cc4ffb4a15777193545a230ca0a
Jaehyun Yoon <yuchem2@gmail.com>
2026-04-20 20:57:53 +0900

    fix: change constant value to use const keyword

--- lotto-project/src/main/kotlin/lotto/constant/LottoConstants.kt
+++ lotto-project/src/main/kotlin/lotto/constant/LottoConstants.kt
@@ -1,7 +1,7 @@
 package lotto.constant
 
-val TICKET_PRICE = 1000
-val MIN_NUMBER = 1
-val MAX_NUMBER = 45
-val TICKET_SIZE = 6
-val BONUS_COUNT = 1
+const val TICKET_PRICE = 1000
+const val MIN_NUMBER = 1
+const val MAX_NUMBER = 45
+const val TICKET_SIZE = 6
+const val BONUS_COUNT = 1
