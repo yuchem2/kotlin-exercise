e1a5ae3532142673f126ea051d8f8f4b65216d0f
Jaehyun Yoon <yuchem2@gmail.com>
2026-04-24 01:01:45 +0900

    chore: remove dotenv file

--- lotto-project/src/main/kotlin/lotto/constant/LottoConstants.kt
+++ lotto-project/src/main/kotlin/lotto/constant/LottoConstants.kt
@@ -1,16 +1,8 @@
 package lotto.constant
 
-import io.github.cdimascio.dotenv.dotenv
-
-val env =
-    dotenv {
-        directory = "./"
-        ignoreIfMissing = true
-    }
-
 const val TICKET_PRICE = 1000
 const val MIN_NUMBER = 1
 const val MAX_NUMBER = 45
 const val TICKET_SIZE = 6
 const val BONUS_COUNT = 1
-val LOTTO_STORE_PATH = env["LOTTO_STORE_PATH"] ?: "lotto.json"
+val LOTTO_STORE_PATH = System.getenv("LOTTO_STORE_PATH") ?: "lotto.json"
