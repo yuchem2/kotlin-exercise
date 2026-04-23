e77b89f6c2019a6a833a296ebc7dd39a12790d29
Jaehyun Yoon <yuchem2@gmail.com>
2026-04-24 00:58:09 +0900

    chore: add .env file for file path

--- lotto-project/build.gradle.kts
+++ lotto-project/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 }
 
 kotlin {
--- lotto-project/src/main/kotlin/lotto/constant/LottoConstants.kt
+++ lotto-project/src/main/kotlin/lotto/constant/LottoConstants.kt
@@ -1,8 +1,16 @@
 package lotto.constant
 
+import io.github.cdimascio.dotenv.dotenv
+
+val env =
+    dotenv {
+        directory = "./"
+        ignoreIfMissing = true
+    }
+
 const val TICKET_PRICE = 1000
 const val MIN_NUMBER = 1
 const val MAX_NUMBER = 45
 const val TICKET_SIZE = 6
 const val BONUS_COUNT = 1
-const val LOTTO_STORE_PATH = "lotto.json"
+val LOTTO_STORE_PATH = env["LOTTO_STORE_PATH"] ?: "lotto.json"
