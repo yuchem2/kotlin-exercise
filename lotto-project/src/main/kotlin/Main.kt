f231639ef0715a045c8d4deee1e0087384cb0657
Jaehyun Yoon <yuchem2@gmail.com>
2026-04-24 01:12:42 +0900

    chore: add encoding setting in main

--- lotto-project/src/main/kotlin/Main.kt
+++ lotto-project/src/main/kotlin/Main.kt
@@ -10,7 +10,14 @@ import lotto.view.ConsoleOutput
 import lotto.view.InputView
 import lotto.view.OutputView
 
+private fun setupEncoding() {
+    System.setOut(java.io.PrintStream(System.out, true, "UTF-8"))
+    System.setErr(java.io.PrintStream(System.err, true, "UTF-8"))
+}
+
 fun main() {
+    setupEncoding()
+
     val account = Account(0)
     val inputView = InputView(ConsoleInput())
     val outputView = OutputView(ConsoleOutput())
