18ccb53679d6c7aba6a02d8d90d252045d5430ce
Jaehyun Yoon <yuchem2@gmail.com>
2026-04-25 17:27:49 +0900

    refactor: change to kotlin style code
    - use private setter for Account
    
    - simplify code in setResult

--- lotto-project/src/main/kotlin/lotto/model/Account.kt
+++ lotto-project/src/main/kotlin/lotto/model/Account.kt
@@ -1,8 +1,11 @@
 package lotto.model
 
 class Account(
-    private var amount: Long,
+    amount: Long
 ) {
+    var amount: Long = amount
+        private set
+
     fun withdraw(amount: Long) {
         require(amount > 0) { "출금액은 0보다 커야 합니다." }
         require(this.amount >= amount) { "잔액보다 출금액이 많습니다." }
@@ -13,6 +16,4 @@ class Account(
         require(amount > 0) { "입금액은 0보다 커야 합니다." }
         this.amount += amount
     }
-
-    fun getAmount() = amount
 }
