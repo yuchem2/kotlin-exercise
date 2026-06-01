9e9d5aacd6c4eac51aef5d73df9b860465d954e6
Jaehyun Yoon <yuchem2@gmail.com>
2026-06-01 18:47:32 +0900

    chore: migration to kotest 6.1.11

--- lotto-project/build.gradle.kts
+++ lotto-project/build.gradle.kts
@@ -23,9 +23,9 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 
-    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
-    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
-    testImplementation("io.kotest:kotest-property:5.9.1")
+    testImplementation("io.kotest:kotest-runner-junit6:6.1.11")
+    testImplementation("io.kotest:kotest-assertions-core:6.1.11")
+    testImplementation("io.kotest:kotest-property:6.1.11")
     testImplementation("io.mockk:mockk:1.13.13")
 }
 
